f19bb3eddad537f6b0a6d3c309018d5b553877f1
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-07-22 02:36:07 -0500

    current weather code done, errors to be fixed

--- Assets/script.js
+++ Assets/script.js
@@ -27,42 +27,56 @@ let weather = {
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed)
         document.querySelector("#city").innerText = name;
-        document.querySelector()
-
+        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+        document.querySelector("#temp").innerText = temp + "°F";
+        document.querySelector("#humidity").innerText = "Humidity" + humidity + "%";
+        document.querySelector("#wind").innerText = "Wind" + speed + "mi/h";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
     }
-}
+};
+
+document.querySelector(".search button")
+addEventListener("click", function () {
+    weather.search();
+
+});
+
+
+
 // https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=0d205d676089b89aaf4fd5a075b4c7d6
 var apiKey2 = '0d205d676089b89aaf4fd5a075b4c7d6'
-var userSearch = document.getElementById('userInput').value;
-var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-var geoAPIurl=`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=5&appid=${apiKey}`
-// var testUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
-// Value from search form
-var lat;
-var lon;
+// var userSearch = document.getElementById('userInput').value;
+// var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+// var geoAPIurl=`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=5&appid=${apiKey}`
+// // var testUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
+// // Value from search form
+// var lat;
+// var lon;
 
-// document.getElementById("#searchTerm").value
-function getLatLon(){
+// // document.getElementById("#searchTerm").value
+// function getLatLon(){
 
-fetch(geoAPIurl)
-.then(function(response){
-return response.json();
-}).then(function(data){
-    console.log(data);
-    console.log("lat",data[0].lat)
-    console.log("lon",data[0].lon)
-    lat = data[0].lat
-    lon = data[0].lon
-    currentForecast();
-})
-};
+// fetch(geoAPIurl)
+// .then(function(response){
+// return response.json();
+// }).then(function(data){
+//     console.log(data);
+//     console.log("lat",data[0].lat)
+//     console.log("lon",data[0].lon)
+//     lat = data[0].lat
+//     lon = data[0].lon
+//     currentForecast();
+// })
+// };
 
-function currentForecast(){
-fetch(weatherAPIUrl)
-.then(function(response){
-return response.json();
-}).then(function(data){
-    console.log(data);
-    // APPEND TO PAGE   
-})
-};
+// function currentForecast(){
+// fetch(weatherAPIUrl)
+// .then(function(response){
+// return response.json();
+// }).then(function(data){
+//     console.log(data);
+//     // APPEND TO PAGE   
+// })
+// };
