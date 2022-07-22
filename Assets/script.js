f70f65bf5440d1413a761b8029dd7edd82c05b12
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-07-22 01:31:34 -0500

    api call for current weather function

--- Assets/script.js
+++ Assets/script.js
@@ -8,8 +8,31 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+let weather = {
+    "apiKey": "0d205d676089b89aaf4fd5a075b4c7d6",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" + this.apiKey
+            
+            ).then((response) => response.json())
+            // .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity} = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector("#city").innerText = name;
+        document.querySelector()
 
-var apiKey = 'd91f911bcf2c0f925fb6535547a5ddc9'
+    }
+}
+// https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=0d205d676089b89aaf4fd5a075b4c7d6
+var apiKey2 = '0d205d676089b89aaf4fd5a075b4c7d6'
 var userSearch = document.getElementById('userInput').value;
 var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
 var geoAPIurl=`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=5&appid=${apiKey}`
